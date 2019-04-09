c12a266984fb331821cad1eb4583dd22d3531078
hyungu kang <hgkang822@gmail.com>
2019-04-09 08:43:57 +0000

    190409 | insta-image upload

--- django/insta/posts/models.py
+++ django/insta/posts/models.py
@@ -1,5 +1,17 @@
 from django.db import models
+from imagekit.models import ProcessedImageField
+from imagekit.processors import ResizeToFill
 
 # Create your models here.
 class Post(models.Model):
     content=models.TextField()
+    # image=models.ImageField(blank=True) #빈 값이 들어갈 수 있도록 blank=True
+    image=ProcessedImageField(
+        upload_to='posts/images', #저장위치
+        processors=[ResizeToFill(600,600)], #처리할 작업 목록
+        format='JPEG', #저장포맷
+        options={'quality':90}, #옵션
+        )
+    
+    #이미지 사용하고 싶을 때:pip install pillow
+    #이미지 편집하고 싶을 때:pip install pilkit django-imagekit
--- django/insta/posts/views.py
+++ django/insta/posts/views.py
@@ -8,7 +8,7 @@ def list(request):
 
 def create(request):
     if request.method=='POST':
-        post_form=PostForm(request.POST)
+        post_form=PostForm(request.POST, request.FILES) #순서 중요
         if post_form.is_valid():
             post_form.save()
             return redirect('posts:list')
@@ -16,8 +16,20 @@ def create(request):
         post_form=PostForm()
     return render(request, 'posts/create.html', {'post_form':post_form})
     
+def update(request, post_id):
+    post=get_object_or_404(Post, id=post_id)
+    if request.method=='POST':
+        post_form=PostForm(request.POST, request.FILES, instance=post)
+        if post_form.is_valid():
+            post_form.save()
+            return redirect('posts:list')
+    else:
+        post_form=PostForm(instance=post) #수정하는 것이므로 위에서 가져온 정보들을 자동으로 폼에 바로 채워넣기 위해 사용(instance)
+    return render(request, 'posts/create.html', {'post_form':post_form})
+    
 def delete(request, post_id):
     # post=Post.objects.get(id=post_id) #pk=post_id도 가능
     post=get_object_or_404(Post, id=post_id)
     post.delete()
     return redirect('posts:list')
+    
