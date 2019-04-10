28f604f5a874b430c7721270f1921f154bb10500
hyungu kang <hgkang822@gmail.com>
2019-04-10 06:49:20 +0000

    190410 | Insta-user

--- django/insta/posts/models.py
+++ django/insta/posts/models.py
@@ -1,13 +1,22 @@
 from django.db import models
 from imagekit.models import ProcessedImageField
 from imagekit.processors import ResizeToFill
+from django.conf import settings
 
-# Create your models here.
+#추가적인 저장 위치 만드는 함수
+def post_image_path(instance, filename):
+    return 'posts/{}/{}' .format(instance.content, filename)
+    # return f'posts/images/{filename}' 권장
+    # return f'posts/{instance.content}/{filename}' 가능 / filename은 확장자가 같이 들어있다
+    # return f'posts/{instance.content}/{instance.content}.jpg' 파일명, 확장자 변경하면서 가능
+    
 class Post(models.Model):
+    user=models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE) #일대일로 가지고 있던 정보들을 없앨 때/cascade:1이 사라지면 N에 있던 가지고 있던 모든 정보 삭제
     content=models.TextField()
     # image=models.ImageField(blank=True) #빈 값이 들어갈 수 있도록 blank=True
     image=ProcessedImageField(
-        upload_to='posts/images', #저장위치
+        # upload_to='posts/images', #저장위치
+        upload_to=post_image_path,
         processors=[ResizeToFill(600,600)], #처리할 작업 목록
         format='JPEG', #저장포맷
         options={'quality':90}, #옵션
--- django/insta/posts/views.py
+++ django/insta/posts/views.py
@@ -1,23 +1,29 @@
 from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.decorators import login_required
 from .forms import PostForm
 from .models import Post
 
 def list(request):
-    posts=Post.objects.all()
+    posts=Post.objects.order_by('-id').all() #정렬할 때 order_by/'-'붙이면 내림차순
     return render(request, 'posts/list.html', {'posts':posts})
 
+@login_required
 def create(request):
     if request.method=='POST':
         post_form=PostForm(request.POST, request.FILES) #순서 중요
         if post_form.is_valid():
-            post_form.save()
+            post=post_form.save(commit=False)
+            post.user=request.user
+            post.save() #실제 DB에 저장
             return redirect('posts:list')
     else: #get요청일때
         post_form=PostForm()
-    return render(request, 'posts/create.html', {'post_form':post_form})
+    return render(request, 'posts/form.html', {'post_form':post_form})
     
 def update(request, post_id):
     post=get_object_or_404(Post, id=post_id)
+    if post.user!=request.user:
+        return redirect('posts:list')
     if request.method=='POST':
         post_form=PostForm(request.POST, request.FILES, instance=post)
         if post_form.is_valid():
@@ -25,11 +31,15 @@ def update(request, post_id):
             return redirect('posts:list')
     else:
         post_form=PostForm(instance=post) #수정하는 것이므로 위에서 가져온 정보들을 자동으로 폼에 바로 채워넣기 위해 사용(instance)
-    return render(request, 'posts/create.html', {'post_form':post_form})
+    return render(request, 'posts/form.html', {'post_form':post_form})
     
 def delete(request, post_id):
     # post=Post.objects.get(id=post_id) #pk=post_id도 가능
     post=get_object_or_404(Post, id=post_id)
+    if post.user!=request.user:
+        return redirect('posts:list')
     post.delete()
+    # if post.user==request.user:
+    #     post.delete()
     return redirect('posts:list')
     
